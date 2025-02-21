fbdf15a93bf2ff2b1c85ae06b073f3ce222a6d27
Boris Kalapchiev <164948528+borizoto@users.noreply.github.com>
2025-02-21 18:15:35 +0200

    Delete Exams/BackEnd - Exam_TechStore directory

--- Exams/BackEnd - Exam_TechStore/src/config.js
+++
@@ -1,7 +0,0 @@
-const JWT_SECRET = '0zqkGbxHL/tCBPfZid5BkFUxEUEG3/tSJlPjb39BsHGgRGD3/GpkwTnSFmE=';
-const AUTH_COOKIE_NAME = 'auth';
-
-export {
-    JWT_SECRET,
-    AUTH_COOKIE_NAME
-};
--- Exams/BackEnd - Exam_TechStore/src/helpers/hbsHelpers.js
+++
@@ -1,5 +0,0 @@
-function setTitle(title) {
-    this.pageTitle = title;
-}
-
-export {setTitle};
